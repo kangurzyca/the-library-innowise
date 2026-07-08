b2785f3b0849e67948457acc7068807011cbd76b
kangurzyca <maciek.ukalski@gmail.com>
2026-07-08 11:09:18 +0200

    Books that are already faved now will have highlighted fave icons once search returns them

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const btnSearch = document.querySelector(".search__button");
 btnSearch.addEventListener("click", handleSearchBar);
-const loader = document.getElementById("list__loader");
 
+const loader = document.getElementById("list__loader");
 const itemName = "theLibraryMyFavedBooks";
-displayFavedBooks(readFavedBooks());
+
+//display faves in a sidebar if they exist
+createBooksCards(readFavedBooks(), "small");
 
 async function handleSearchBar(e) {
   const searchBar = document.querySelector(".search__input");
@@ -51,7 +53,7 @@ function createBooksCards(booksArray, size) {
       : `https://covers.openlibrary.org/b/id/${el.cover_i}.jpg`;
     const author = size ? el.author : el.author_name.join(", ");
 
-    const favedClass = ".book-card__icon--faved"
+    const favedClass = size ? "book-card__icon--faved" : ""
 
     booksList.innerHTML += `
        <li class="book-card"
@@ -59,11 +61,9 @@ function createBooksCards(booksArray, size) {
        data-author="${author}"
        data-first_publish_year="${el.first_publish_year}"
        data-coverurl="https://covers.openlibrary.org/b/id/${el.cover_i}.jpg">
-        <button class="book-card__fave-button">
-
-          <img class="book-card__icon" src="./src/assets/heart.svg" alt="heart icon">
-        </button>
-
+          <button class="book-card__fave-button">
+            <img class="book-card__icon ${favedClass}" src="./src/assets/heart.svg" alt="heart icon">
+          </button>
           <img class="book-card__image" src="${coverurl}">
           <h3 class="book-card__title">${el.title}</h3>
           <p class="book-card__author">${author}</p>
@@ -74,38 +74,54 @@ function createBooksCards(booksArray, size) {
   booksList
     .querySelectorAll(".book-card__fave-button")
     .forEach((el) => el.addEventListener("click", (e) => faveTheBook(e)));
+
+    // checking if fetched books contain already faved ones
+  const favedBooks = readFavedBooks()
+  booksList.querySelectorAll("li").forEach(li=>{ //iterate over booksList items
+    favedBooks.forEach(book=>{ //for each booksList item iterate over faved
+      if(book.title === li.dataset.title && // check if there is a match
+      book.author === li.dataset.author &&
+      book.first_publish_year === li.dataset.first_publish_year){
+        li.querySelector(".book-card__icon").classList.add("book-card__icon--faved") // add a faved class
+      }
+    })
+  })
+
 }
 function faveTheBook(e) {
-  console.log("initialized faving");
-
+  // get faved books from localStorage
   let myFavedBooks = readFavedBooks();
+  //remove item from localStorage before its updated and stored back
   localStorage.removeItem(itemName);
-  console.log("my books: ", myFavedBooks);
 
+  //get data from a li element of booked to be faved
   const newFavedBook = {
     title: e.target.parentElement.dataset.title,
     author: e.target.parentElement.dataset.author,
     first_publish_year: e.target.parentElement.dataset.first_publish_year,
     coverurl: e.target.parentElement.dataset.coverurl,
   };
+  // check if book to be faved is already stored in localStorage
   const isAlreadyStored = myFavedBooks.findIndex((book) => {
-    console.log("comparing faves");
     return (
       book.title === newFavedBook.title &&
       book.author === newFavedBook.author &&
       book.first_publish_year === newFavedBook.first_publish_year
     );
   });
-  console.log(isAlreadyStored);
 
+  //deciding whether to fave or not basing on findIndex return value => -1 if not found or actuall item index
   if (isAlreadyStored !== -1) {
-    console.log("unfaving");
     myFavedBooks.splice(isAlreadyStored, 1);
+    e.target.querySelector("img").classList.remove("book-card__icon--faved");
   } else {
     myFavedBooks.push(newFavedBook);
+    e.target.querySelector("img").classList.add("book-card__icon--faved");
   }
+  //setting updated item in localStorage
   localStorage.setItem(itemName, JSON.stringify(myFavedBooks));
-  displayFavedBooks(readFavedBooks());
+  // rerendering aside faved list
+  createBooksCards(readFavedBooks(), "small")
 }
 
 function readFavedBooks() {
@@ -116,20 +132,16 @@ function readFavedBooks() {
   return myFavedBooks;
 }
 
-function displayFavedBooks(booksArray) {
-  createBooksCards(booksArray, "small");
-}
+// laoding state function
 function setLoading(isLoading) {
   if (isLoading) {
+    //delete current booksList if a new search is called
     document.querySelector(".books__list").innerHTML = "";
-
+    //show a loader message
     loader.style.display = "block";
-
-    console.log("loading books");
   } else if (!isLoading) {
+    //hide loading message
     loader.style.display = "none";
-
-    console.log("books loaded");
   }
 }
 
